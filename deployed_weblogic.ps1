2c196d4dd521b4d620b049fee17098aa937bae70
Akbaralishaikh-devop <137177751+Akbaralishaikh-devop@users.noreply.github.com>
2023-08-09 16:04:31 +0530

    Update deployed_weblogic.ps1

--- deployed_weblogic.ps1
+++ deployed_weblogic.ps1
@@ -1,7 +1,7 @@
 ﻿
 # Set the variables for the deployement
 #USE t3 INSTEAD OF http IF YOU HAVE NOT ENABLED THE TUNNELING. IF YOU WANT TO ENABLE TUNNELING IN SERVER>PROTOCALS>GENERAL>ENABLE TUNNELING
-$adminUrl = "http://192.168.10.195:7001"
+$adminUrl = "http://192.168.5.155:7001"
 $username = "weblogic"
 $password = "weblogic@123"
 $appPath = "C:\Users\AKBARALI\.jenkins\workspace\gradle\build\libs\demo.war"
