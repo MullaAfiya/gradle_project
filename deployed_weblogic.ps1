ead5db243e1c37567340c1b3dd33b07b089287fd
MullaAfiya <135946533+MullaAfiya@users.noreply.github.com>
2023-08-10 12:58:25 +0530

    Update deployed_weblogic.ps1

--- deployed_weblogic.ps1
+++ deployed_weblogic.ps1
@@ -17,5 +17,6 @@ java -cp "D:\devops\oracle\wlserver\server\lib\weblogic.jar" weblogic.Deployer `
  -password $password `
  -deploy $appPath `
  -targets $target `
+ -name demo
 
 Write-Host "Application Deployed successfully."
